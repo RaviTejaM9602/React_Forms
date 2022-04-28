0cd12ca61b934235914d08de1a750c717bfed13a
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-04-28 12:05:18 +0100

    update code for radio button

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -100,6 +100,23 @@ export default function Form() {
             <br />
             
         </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
     </form>
 )
 }
